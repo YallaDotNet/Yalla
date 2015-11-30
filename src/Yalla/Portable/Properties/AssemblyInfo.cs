516af05ab97fe9efe1fa200cf9776327db48e911
Dmitry Shechtman <damnian@gmail.com>
2015-11-30 23:39:16 +0200

    Add COM visibility

--- src/Yalla/Portable/Properties/AssemblyInfo.cs
+++ src/Yalla/Portable/Properties/AssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("YALLA.NET")]
 [assembly: AssemblyDescription("Yet Another Logging Library Abstraction for .NET")]
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: CLSCompliant(true)]
+
+#if !(PORTABLE && NET40)
+[assembly: ComVisible(true)]
+#endif
