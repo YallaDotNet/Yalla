106e729f077f5619bd9bec98dc221d332bc9a502
Dmitry Shechtman <damnian@gmail.com>
2015-11-30 23:37:49 +0200

    Extract YallaAssemblyInfo.cs

--- src/Yalla/Portable/Properties/AssemblyInfo.cs
+++ src/Yalla/Portable/Properties/AssemblyInfo.cs
@@ -1,19 +1,7 @@
-﻿using System.Resources;
+﻿using System;
 using System.Reflection;
-using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Yalla")]
+[assembly: AssemblyTitle("YALLA.NET")]
 [assembly: AssemblyDescription("Yet Another Logging Library Abstraction for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dmitry Shechtman")]
-[assembly: AssemblyProduct("YALLA.NET")]
-[assembly: AssemblyCopyright("Copyright © Dmitry Shechtman 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
-
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: CLSCompliant(true)]
